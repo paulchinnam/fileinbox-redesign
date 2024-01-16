aa5a7fa711af8a033a20b47fae8cb03656346c63
Paul Chinnam <63817253+paulchinnam@users.noreply.github.com>
2024-01-16 13:36:53 -0600

    removed blue gradient in cta section

--- src/app/components/CTA.js
+++ src/app/components/CTA.js
@@ -7,7 +7,7 @@ import macbook from "public/macbook2.png";
 export default function CTA() {
   return (
     <div className="relative isolate bg-gray-100">
-      <div
+      {/* <div
         className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
         aria-hidden="true"
       >
@@ -18,7 +18,7 @@ export default function CTA() {
               "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
           }}
         />
-      </div>
+      </div> */}
       <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-16 lg:px-8">
         <div className="relative isolate overflow-hidden ring-1 ring-gray-200 bg-white px-6 pt-16 shadow-md sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
           {/* <svg
